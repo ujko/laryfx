6d0f67282bb44a1531b215f61099fef9d7172caf
Paweł Ujazdowski <uio6398@gmail.com>
2017-10-10 22:23:53 +0200

    Lary project

---
+++ src/main/java/lary/controllers/MainController.java
@@ -0,0 +1,10 @@
+package lary.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+public class MainController {
+
+    @FXML
+    public BorderPane mainBorderPane;
+}
