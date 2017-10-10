72edb5ef8732a50f52ef0619de46b678a45e4252
Paweł Ujazdowski <uio6398@gmail.com>
2017-10-10 23:20:49 +0200

    Added class ConnectionParam

--- src/main/java/lary/controllers/ConnectionSettingsController.java
+++ src/main/java/lary/controllers/ConnectionSettingsController.java
@@ -1,4 +1,23 @@
 package lary.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class ConnectionSettingsController {
+    @FXML
+    public ListView connectionsListView;
+    @FXML
+    public TextField connName;
+    @FXML
+    public TextField connUser;
+    @FXML
+    public PasswordField connPassword;
+    @FXML
+    public TextField connServer;
+    @FXML
+    public TextField connPort;
+    @FXML
+    public TextField connServis;
 }
---
+++ src/main/java/lary/model/ConnectionParam.java
@@ -0,0 +1,86 @@
+package lary.model;
+
+public class ConnectionParam {
+    private String connName;
+    private String connUser;
+    private String connPass;
+    private String connServer;
+    private String connPort;
+    private String connService;
+
+    private ConnectionParam(ConnectionParamBuilder cpb) {
+        this.connName = cpb.connName;
+        this.connUser = cpb.connUser;
+        this.connPass = cpb.connPass;
+        this.connServer = cpb.connServer;
+        this.connPort = cpb.connPort;
+        this.connService = cpb.connService;
+    }
+
+    public String getConnName() {
+        return connName;
+    }
+
+    public String getConnUser() {
+        return connUser;
+    }
+
+    public String getConnPass() {
+        return connPass;
+    }
+
+    public String getConnServer() {
+        return connServer;
+    }
+
+    public String getConnPort() {
+        return connPort;
+    }
+
+    public String getConnService() {
+        return connService;
+    }
+
+    public static class ConnectionParamBuilder {
+        private String connName;
+        private String connUser;
+        private String connPass;
+        private String connServer;
+        private String connPort;
+        private String connService;
+
+        public ConnectionParamBuilder connName(String connName) {
+            this.connName = connName;
+            return this;
+        }
+
+        public ConnectionParamBuilder connUser(String connUser) {
+            this.connUser = connUser;
+            return this;
+        }
+
+        public ConnectionParamBuilder connPass(String connPass) {
+            this.connPass = connPass;
+            return this;
+        }
+
+        public ConnectionParamBuilder connServer(String connServer) {
+            this.connServer = connServer;
+            return this;
+        }
+
+        public ConnectionParamBuilder connPort(String connPort) {
+            this.connPort = connPort;
+            return this;
+        }
+
+        public ConnectionParamBuilder connService(String connService) {
+            this.connService = connService;
+            return this;
+        }
+
+        public ConnectionParam build() {
+            return new ConnectionParam(this);
+        }
+    }
+}
